fd12bf93c099d6e939099a81c1dc35757e97d5d7
Jack You <gmyou71@gmail.com>
2017-09-03 19:16:46 +0900

    ui

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, title: 'GIS Viewer'})
   mainWindow.maximize()
 
   // and load the index.html of the app.
